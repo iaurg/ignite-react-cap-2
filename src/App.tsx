c59e59354b4c439cfef4652efea9471f738662d1
Italo Aurelio <italoaurelior@gmail.com>
2021-03-18 22:00:18 -0300

    Add new components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <GlobalStyle />
+    <>
+      <GlobalStyle />
+      <Header />
+      <Dashboard />
+    </>
   );
 }
