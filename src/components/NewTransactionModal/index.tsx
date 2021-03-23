d862a956e82a66484054ec2059258a828368d36a
Italo Aurelio <italoaurelior@gmail.com>
2021-03-23 20:32:23 -0300

    Styled modal global

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from 'react-modal'
+import closeImg from '../../assets/close.svg'
+import { Container } from './styles'
 
 type NewTransactionModalProps = {
   isModalOpen: boolean
@@ -10,8 +12,22 @@ export function NewTransactionModal({isModalOpen, onRequestClose}:NewTransaction
     <Modal 
     isOpen={isModalOpen} 
     onRequestClose={onRequestClose}
+    overlayClassName="react-modal-overlay"
+    className="react-modal-content"
   >
-    <h2>Cadastrar transação</h2>
+    <button type="button" onClick={onRequestClose} className="react-modal-close"> 
+      <img src={closeImg} alt="Close Modal" />
+    </button>
+    <Container>
+      <h2>Cadastrar transação</h2>
+
+      <input type="text" name="" id=""placeholder="Título"/>
+      <input type="number" name="" id="" placeholder="Valor"/>
+      <input type="text" name="" id="" placeholder="Categoria"/>
+      <button type="submit">
+        Cadastrar
+      </button>
+    </Container>
   </Modal>
   )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,6 +12,7 @@ export const GlobalStyle = createGlobalStyle`
     --text-body: #969cb3;
 
     --background: #f0f2f5;
+    --input-background: #e7e9ee;
     --shape: #fff;
   }
 
@@ -52,5 +53,42 @@ export const GlobalStyle = createGlobalStyle`
   [disabled] {
     opacity:0.6;
     cursor:not-allowed;
+  }
+
+  .react-modal-overlay {
+    background: rgba(0,0,0,0.5);
+
+    position:fixed;
+    top:0;
+    bottom:0;
+    left:0;
+    right:0;
+
+    display:flex;
+    justify-content:center;
+    align-items:center;
+  }
+
+  .react-modal-content {
+    width:100%;
+    max-width:576px;
+    background:var(--background);
+    padding:3rem;
+    position:relative;
+    border-radius:0.25rem;
+    border:none;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right:1.5rem;
+    top:1.5rem;
+    border:0;
+    background: transparent;
+    transition: filter 0.2s;
+    
+    &:hover{
+      filter:brightness(0.8)
+    }
   }
 `
