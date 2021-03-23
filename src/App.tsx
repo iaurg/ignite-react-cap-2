5c8f62b66e58429fd65609ccf5ef974a4bd4d5a3
Italo Aurelio <italoaurelior@gmail.com>
2021-03-23 19:50:32 -0300

    Configure miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { createServer } from "miragejs"
 import { GlobalStyle } from "./styles/global";
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          value: 1000
+        }
+      ]
+    })
+  }
+})
 
 export function App() {
   return (
