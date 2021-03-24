b4452dbc60c60fa7057c968ec73230980bef78f2
Italo Aurelio <italoaurelior@gmail.com>
2021-03-24 07:07:32 -0300

    Populate table

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,29 @@ createServer({
   models: {
     transaction: Model
   },
+  
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelancer website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1000,
+          createdAt: new Date('2021-02-11 09:00:00')
+        }
+      ]
+    })
+  },
 
   routes(){
     this.namespace = 'api';
